765e533b11599db0d86edc1a1c23c2506d0aec50
Miguel Silveira <miguel.silveira@eqs-global.com>
2018-03-21 17:21:57 +0000

    Primitives

---
+++ src/app/objects/Primitives.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import * as THREE from 'three';
+
+@Injectable()
+export class Cube {
+
+  private _geometry: THREE.BoxGeometry;
+  private _material: THREE.MeshBasicMaterial;
+
+  get geometry(): THREE.BoxGeometry {
+    return this._geometry;
+  }
+
+  set geometry(geometry: THREE.BoxGeometry) {
+    this._geometry = geometry;
+  }
+
+  get material(): THREE.MeshBasicMaterial {
+    return this._material;
+  }
+
+  set material(material: THREE.MeshBasicMaterial) {
+    this._material = material;
+  }
+ 
+  constructor(geometry?: THREE.BoxGeometry, material?: THREE.MeshBasicMaterial) {
+    this._geometry = geometry ? geometry : new THREE.BoxGeometry(10, 10, 10);
+    this._material = material ? material : new THREE.MeshBasicMaterial({ color : 0xFFFFFF, wireframe: true });
+  }
+
+}
